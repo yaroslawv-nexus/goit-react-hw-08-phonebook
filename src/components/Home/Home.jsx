dfeb4149cfa6be148ed02fd9f64e2827ec25481c
yaroslawv-nexus <yaroslawv@gmail.com>
2023-10-15 15:18:04 +0300

    fix

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TitleHome } from './Home.styled';
+
+const Home = () => {
+  return (
+    <div>
+      <TitleHome>Welcome to Contact App</TitleHome>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Home/Home.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const TitleHome = styled.h1`
+font-size: 50px;
+
+`
